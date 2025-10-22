b9ad1a12c06e4f31266f1d087320f35ebed81a31
Jan Brabec <brabej16@cvut.cz>
2025-10-22 18:57:21 +0200

    Fix: resolved PlayerData merge confict

--- just_one_more/Assets/Scripts/PlayerAndEnemies/PlayerData.cs
+++ just_one_more/Assets/Scripts/PlayerAndEnemies/PlayerData.cs
@@ -1,4 +1,3 @@
-<<<<<<<< HEAD:just_one_more/Assets/Scripts/PlayerData.cs
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "NewPlayerData", menuName = "Scriptable Objects/NewPlayerData")]
@@ -13,21 +12,4 @@ public class PlayerData : ScriptableObject
     public int knockback = 5;
     public bool isDead = false;
     public bool isMelee = false;
-
 }
-========
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "NewPlayerData", menuName = "Scriptable Objects/NewPlayerData")]
-public class PlayerData : ScriptableObject
-{
-    public int hp = 100;
-    public int moveSpeed = 5;
-    public int attackSpeed = 1;
-    public int damage = 10;
-    public int money = 0;
-    public int bulletSpeed = 10;
-    public bool isDead = false;
-
-}
->>>>>>>> origin/main:just_one_more/Assets/Scripts/PlayerAndEnemies/PlayerData.cs
